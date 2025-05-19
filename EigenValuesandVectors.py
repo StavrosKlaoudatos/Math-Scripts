82572d26548782fab3efce8d5b44f7d9b1aca82c
Stavros Klaoudatos <62154974+StavrosKlaoudatos@users.noreply.github.com>
2025-05-18 18:29:33 -0700

    Update EigenValuesandVectors.py

--- EigenValuesandVectors.py
+++ EigenValuesandVectors.py
@@ -1,12 +1,1 @@
-def FindEigenValuesandVectorsofMatrixorOperator(matrix):
-    import math
-    import numpy as np
-    import scipy.linalg as la
-
-    eigvals, eigvecs = la.eig(matrix)
-    print("EigenValues: ",eigvals)
-    print("EigenVectors: \n",eigvecs)
-
-
-
 
